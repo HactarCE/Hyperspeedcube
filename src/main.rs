edaf2b1854d7f2af9f733083566b67e8bde4ef46
HactarCE <6060305+HactarCE@users.noreply.github.com>
2021-10-25 21:16:00 -0400

    Update depedencies

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,15 @@ extern crate glium;
 extern crate lazy_static;
 
 use core::cell::RefCell;
-use glium::glutin;
+use glium::glutin::{
+    event::{ElementState, Event, KeyboardInput, StartCause, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+    ContextBuilder,
+};
 use send_wrapper::SendWrapper;
-use std::time;
+use std::collections::VecDeque;
+use std::time::{Duration, Instant};
 
 pub mod puzzle;
 mod render;
@@ -22,81 +28,132 @@ use puzzle::{rubiks3d::twists, PuzzleEnum, PuzzleType};
 /// The title of the window.
 const TITLE: &str = "Keyboard Speedcube";
 
+const FPS: f64 = 60.0;
+
 lazy_static! {
-    static ref EVENTS_LOOP: SendWrapper<RefCell<glutin::EventsLoop>> =
-        SendWrapper::new(RefCell::new(glutin::EventsLoop::new()));
+    static ref FRAME_DURATION: Duration = Duration::from_secs_f64(1.0 / FPS);
+    static ref EVENTS_LOOP: SendWrapper<RefCell<Option<EventLoop<()>>>> =
+        SendWrapper::new(RefCell::new(Some(EventLoop::new())));
     static ref DISPLAY: SendWrapper<glium::Display> = SendWrapper::new({
-        let wb = glutin::WindowBuilder::new().with_title(TITLE.to_owned());
-        let cb = glutin::ContextBuilder::new().with_vsync(true);
-        glium::Display::new(wb, cb, &EVENTS_LOOP.borrow()).expect("Failed to initialize display")
+        let wb = WindowBuilder::new().with_title(TITLE.to_owned());
+        let cb = ContextBuilder::new().with_vsync(true);
+        glium::Display::new(wb, cb, EVENTS_LOOP.borrow().as_ref().unwrap())
+            .expect("Failed to initialize display")
     });
 }
 
 fn main() {
+    // Initialize runtime data.
     let mut puzzle = PuzzleType::Rubiks3D.new();
+    let mut events_buffer = VecDeque::new();
 
     render::setup_puzzle(puzzle.puzzle_type());
 
-    let mut last_frame = time::Instant::now();
-    let mut closed = false;
-    while !closed {
-        // Handle events.
-        EVENTS_LOOP.borrow_mut().poll_events(|ev| match ev {
-            glutin::Event::WindowEvent { event, .. } => match event {
-                // Handle window close event.
-                glutin::WindowEvent::CloseRequested => closed = true,
-                glutin::WindowEvent::KeyboardInput { input, .. } => match input {
-                    glutin::KeyboardInput {
-                        state: glutin::ElementState::Pressed,
-                        virtual_keycode: Some(keycode),
-                        ..
+    // Main loop
+    let mut last_frame_time = Instant::now();
+    let mut next_frame_time = Instant::now();
+    EVENTS_LOOP
+        .borrow_mut()
+        .take()
+        .unwrap()
+        .run(move |event, _ev_loop, control_flow| {
+            // Handle events.
+            let mut now = Instant::now();
+            let mut do_frame = false;
+
+            match event.to_static() {
+                Some(Event::NewEvents(cause)) => match cause {
+                    StartCause::ResumeTimeReached {
+                        start: _,
+                        requested_resume,
                     } => {
-                        use glutin::VirtualKeyCode as Vk;
-                        match &mut puzzle {
-                            PuzzleEnum::Rubiks3D(cube) => match keycode {
-                                Vk::Escape => closed = true,
-                                Vk::U => cube.twist(*twists::R),
-                                Vk::E => cube.twist(twists::R.rev()),
-                                Vk::L => cube.twist(twists::R.fat()),
-                                Vk::M => cube.twist(twists::R.fat().rev()),
-                                Vk::N => cube.twist(*twists::U),
-                                Vk::T => cube.twist(twists::U.rev()),
-                                Vk::S => cube.twist(*twists::L),
-                                Vk::F => cube.twist(twists::L.rev()),
-                                Vk::V => cube.twist(twists::L.fat()),
-                                Vk::P => cube.twist(twists::L.fat().rev()),
-                                Vk::R => cube.twist(*twists::D),
-                                Vk::I => cube.twist(twists::D.rev()),
-                                Vk::H => cube.twist(*twists::F),
-                                Vk::D => cube.twist(twists::F.rev()),
-                                Vk::W => cube.twist(*twists::B),
-                                Vk::Y => cube.twist(twists::B.rev()),
-                                Vk::G | Vk::J => cube.twist(*twists::X),
-                                Vk::B | Vk::K => cube.twist(twists::X.rev()),
-                                Vk::O => cube.twist(*twists::Y),
-                                Vk::A => cube.twist(twists::Y.rev()),
-                                Vk::Semicolon => cube.twist(*twists::Z),
-                                Vk::Q => cube.twist(twists::Z.rev()),
-                                _ => (),
-                            },
-                        }
+                        now = requested_resume;
+                        do_frame = true;
+                    }
+                    StartCause::Init => {
+                        next_frame_time = now;
+                        do_frame = true;
                     }
                     _ => (),
                 },
-                _ => (),
-            },
-            _ => (),
+
+                // The program is about to exit.
+                Some(Event::LoopDestroyed) => (),
+
+                // Queue the event to be handled next time we render
+                // everything.
+                Some(ev) => events_buffer.push_back(ev),
+
+                // Ignore this event.
+                None => (),
+            };
+
+            if do_frame && next_frame_time <= now {
+                next_frame_time = now + *FRAME_DURATION;
+                if next_frame_time < Instant::now() {
+                    // Skip a frame (or several).
+                    next_frame_time = Instant::now() + *FRAME_DURATION;
+                }
+                *control_flow = ControlFlow::WaitUntil(next_frame_time);
+
+                if let Some(delta) = now.checked_duration_since(last_frame_time) {
+                    puzzle.advance(delta);
+                }
+                last_frame_time = now;
+
+                for ev in events_buffer.drain(..) {
+                    match ev {
+                        Event::WindowEvent { event, .. } => match event {
+                            // Handle window close event.
+                            WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                            WindowEvent::KeyboardInput { input, .. } => match input {
+                                KeyboardInput {
+                                    state: ElementState::Pressed,
+                                    virtual_keycode: Some(keycode),
+                                    ..
+                                } => {
+                                    use VirtualKeyCode as Vk;
+                                    match &mut puzzle {
+                                        PuzzleEnum::Rubiks3D(cube) => match keycode {
+                                            Vk::Escape => *control_flow = ControlFlow::Exit,
+                                            Vk::U => cube.twist(*twists::R),
+                                            Vk::E => cube.twist(twists::R.rev()),
+                                            Vk::L => cube.twist(twists::R.fat()),
+                                            Vk::M => cube.twist(twists::R.fat().rev()),
+                                            Vk::N => cube.twist(*twists::U),
+                                            Vk::T => cube.twist(twists::U.rev()),
+                                            Vk::S => cube.twist(*twists::L),
+                                            Vk::F => cube.twist(twists::L.rev()),
+                                            Vk::V => cube.twist(twists::L.fat()),
+                                            Vk::P => cube.twist(twists::L.fat().rev()),
+                                            Vk::R => cube.twist(*twists::D),
+                                            Vk::I => cube.twist(twists::D.rev()),
+                                            Vk::H => cube.twist(*twists::F),
+                                            Vk::D => cube.twist(twists::F.rev()),
+                                            Vk::W => cube.twist(*twists::B),
+                                            Vk::Y => cube.twist(twists::B.rev()),
+                                            Vk::G | Vk::J => cube.twist(*twists::X),
+                                            Vk::B | Vk::K => cube.twist(twists::X.rev()),
+                                            Vk::O => cube.twist(*twists::Y),
+                                            Vk::A => cube.twist(twists::Y.rev()),
+                                            Vk::Semicolon => cube.twist(*twists::Z),
+                                            Vk::Q => cube.twist(twists::Z.rev()),
+                                            _ => (),
+                                        },
+                                    }
+                                }
+                                _ => (),
+                            },
+                            _ => (),
+                        },
+                        _ => (),
+                    }
+                }
+
+                // Render the puzzle.
+                let mut target = DISPLAY.draw();
+                render::draw_puzzle(&mut target, &mut puzzle).expect("Draw error");
+                target.finish().unwrap();
+            }
         });
-
-        {
-            let this_frame = time::Instant::now();
-            puzzle.advance(this_frame - last_frame);
-            last_frame = this_frame;
-        }
-
-        // Render the puzzle.
-        let mut target = DISPLAY.draw();
-        render::draw_puzzle(&mut target, &mut puzzle).expect("Draw error");
-        target.finish().unwrap();
-    }
 }
