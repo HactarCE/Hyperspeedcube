1d417166515c926b60cfcaab37f486f626cd271d
HactarCE <6060305+HactarCE@users.noreply.github.com>
2022-03-07 11:23:43 -0500

    Update config dependency

--- src/preferences/mod.rs
+++ src/preferences/mod.rs
@@ -6,23 +6,21 @@
 use directories::ProjectDirs;
 use enum_map::EnumMap;
 use notify::{DebouncedEvent, RecommendedWatcher, RecursiveMode, Watcher};
-use send_wrapper::SendWrapper;
 use serde::{de, Deserialize, Deserializer, Serialize};
-use std::collections::HashMap;
 use std::error::Error;
 use std::fmt;
-use std::io::Write;
 use std::marker::PhantomData;
 use std::path::PathBuf;
 use std::sync::{mpsc, Mutex, MutexGuard, TryLockError};
 use std::time::Duration;
+use strum::AsRefStr;
 
 mod colors;
 mod info;
 mod keybinds;
 
-use crate::commands::{Command, PuzzleCommand, PuzzleCommandSerde};
-use crate::puzzle::{traits::*, PuzzleType};
+use crate::commands::{Command, PuzzleCommand};
+use crate::puzzle::PuzzleType;
 pub use colors::ColorPreferences;
 pub use info::InfoPreferences;
 pub use keybinds::{GeneralKeybind, Key, KeyCombo, Keybind, PuzzleKeybind};
@@ -30,8 +28,9 @@ pub use keybinds::{GeneralKeybind, Key, KeyCombo, Keybind, PuzzleKeybind};
 const PREFS_FILE_NAME: &str = "hyperspeedcube";
 const PREFS_FILE_EXTENSION: &str = "yaml";
 const PREFS_FILE_FORMAT: config::FileFormat = config::FileFormat::Yaml;
-const DEFAULT_PREFS: &str = include_str!("default.yaml");
+const DEFAULT_PREFS_STR: &str = include_str!("default.yaml");
 
+// File paths
 lazy_static! {
     static ref LOCAL_DIR: Result<PathBuf, PrefsError> = (|| Some(
         // IIFE to mimic `try_block`
@@ -51,7 +50,6 @@ lazy_static! {
             false
         }
     };
-    static ref PREFERENCES: Mutex<Preferences> = Mutex::new(Preferences::load(None));
     static ref PROJECT_DIRS: Option<ProjectDirs> = ProjectDirs::from("", "", "Hyperspeedcube");
     static ref PREFS_FILE_PATH: Result<PathBuf, PrefsError> = {
         let mut p = if *NONPORTABLE {
@@ -67,6 +65,14 @@ lazy_static! {
         p.push(format!("{}.{}", PREFS_FILE_NAME, PREFS_FILE_EXTENSION));
         Ok(p)
     };
+
+}
+
+// Preferences
+lazy_static! {
+    static ref PREFERENCES: Mutex<Preferences> = Mutex::new(Preferences::load(None));
+    pub static ref DEFAULT_PREFS: Preferences =
+        serde_yaml::from_str(DEFAULT_PREFS_STR).unwrap_or_default();
 }
 
 lazy_static! {
@@ -76,7 +82,7 @@ lazy_static! {
             Ok(w) => *WATCHER.lock().unwrap() = Some(w),
             Err(e) => eprintln!("Error initializing preferences file watcher: {}", e),
         }
-        unwatch_during(|| ());
+        unwatch_during(|| ()); // Start watching
 
         Mutex::new(rx)
     };
@@ -134,7 +140,7 @@ enum PrefsError {
 }
 impl Error for PrefsError {}
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
+#[derive(Serialize, Deserialize, Debug, Default, Clone)]
 #[serde(default)]
 pub struct Preferences {
     #[serde(skip)]
@@ -152,74 +158,67 @@ pub struct Preferences {
     pub general_keybinds: Vec<Keybind<Command>>,
     pub puzzle_keybinds: PerPuzzle<Vec<Keybind<PuzzleCommand>>>,
 }
-impl Default for Preferences {
-    fn default() -> Self {
-        Self {
-            needs_save: true,
-            window_states: Default::default(),
-
-            log_file: None,
-
-            gfx: Default::default(),
-            info: Default::default(),
-            view: Default::default(),
-            colors: Default::default(),
-            general_keybinds: Default::default(),
-            puzzle_keybinds: Default::default(),
-        }
-    }
-}
 impl Preferences {
     pub fn load(backup: Option<&Self>) -> Self {
-        let mut config = config::Config::new();
+        let mut config = config::Config::builder();
 
         // Load default preferences.
-        let default_config_source = config::File::from_str(DEFAULT_PREFS, PREFS_FILE_FORMAT);
-        let _ = config.merge(default_config_source.clone());
+        let default_config_source = config::File::from_str(DEFAULT_PREFS_STR, PREFS_FILE_FORMAT);
+        config = config.add_source(default_config_source.clone());
 
         // Load user preferences.
         match &*PREFS_FILE_PATH {
-            Ok(path) => {
-                let _ = config.merge(config::File::from(path.as_ref()));
-            }
+            Ok(path) => config = config.add_source(config::File::from(path.as_ref())),
             Err(e) => eprintln!("Error loading user preferences: {}", e),
         }
 
-        config.try_into::<Self>().unwrap_or_else(|e| {
-            eprintln!("Error loading preferences: {}", e);
-            if let Ok(prefs_path) = &*PREFS_FILE_PATH {
-                let datetime = time::OffsetDateTime::now_local()
-                    .unwrap_or_else(|_| time::OffsetDateTime::now_utc());
-                let mut backup_path = prefs_path.clone();
-                backup_path.pop();
-                backup_path.push(format!(
-                    "{}_{:04}-{:02}-{:02}_{:02}-{:02}-{:02}_bak.{}",
-                    PREFS_FILE_NAME,
-                    datetime.year(),
-                    datetime.month() as u8 ,
-                    datetime.day(),
-                    datetime.hour(),
-                    datetime.minute(),
-                    datetime.second(),
-                    PREFS_FILE_EXTENSION,
-                ));
-                if std::fs::rename(prefs_path, &backup_path).is_ok() {
-                    eprintln!(
-                        "Backup of old preferences stored at {}",
-                        backup_path.to_str().unwrap_or(
-                            "some path with invalid Unicode. Seriously, what have you done to your filesystem?"
-                        ),
-                    );
+        // TODO: use try block (including the word "IIFE" here because I'll
+        // search for that)
+        config
+            .build()
+            .and_then(|c| c.try_deserialize::<Self>())
+            .unwrap_or_else(|e| {
+                eprintln!("Error loading preferences: {}", e);
+                if let Ok(prefs_path) = &*PREFS_FILE_PATH {
+                    let datetime = time::OffsetDateTime::now_local()
+                        .unwrap_or_else(|_| time::OffsetDateTime::now_utc());
+                    let mut backup_path = prefs_path.clone();
+                    backup_path.pop();
+                    backup_path.push(format!(
+                        "{}_{:04}-{:02}-{:02}_{:02}-{:02}-{:02}_bak.{}",
+                        PREFS_FILE_NAME,
+                        datetime.year(),
+                        datetime.month() as u8,
+                        datetime.day(),
+                        datetime.hour(),
+                        datetime.minute(),
+                        datetime.second(),
+                        PREFS_FILE_EXTENSION,
+                    ));
+                    if std::fs::rename(prefs_path, &backup_path).is_ok() {
+                        eprintln!(
+                            "Backup of old preferences stored at {}",
+                            backup_path.to_str().unwrap_or(
+                                "some path with invalid Unicode. Seriously, what have you done to your filesystem?"
+                            ),
+                        );
+                    }
                 }
-            }
 
-            // Try backup
-            backup.cloned()
-            // Try just default config
-.            or_else(||
-                config::Config::new().with_merged(default_config_source).unwrap().try_into().ok()
-            ).unwrap_or_else(Preferences::default)
-        })
+                // Try backup
+                backup
+                    .cloned()
+                    // Try just default config
+                    .or_else(|| {
+                        config::Config::builder()
+                            .add_source(default_config_source)
+                            .build()
+                            .ok()?
+                            .try_deserialize()
+                            .ok()
+                    })
+                    .unwrap_or_default()
+            })
     }
 
     pub fn save(&mut self) {
