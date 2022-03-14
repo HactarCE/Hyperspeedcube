bc0fecf3459e7890bb38fb74324ed8cddad145bb
HactarCE <6060305+HactarCE@users.noreply.github.com>
2022-03-14 17:16:33 -0400

    Add turn metric to default preferences

--- src/preferences/info.rs
+++ src/preferences/info.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 use crate::puzzle::TwistMetric;
 
 #[derive(Serialize, Deserialize, Debug, Default, Clone)]
+#[serde(default)]
 pub struct InfoPreferences {
     pub metric: TwistMetric,
 }
