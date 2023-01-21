ffab028628aa462523207dcfdad98020d04e322b
HactarCE <6060305+HactarCE@users.noreply.github.com>
2023-01-21 04:32:21 -0500

    Add debug label and link to download on web

--- src/gui/menu_bar.rs
+++ src/gui/menu_bar.rs
@@ -124,5 +124,12 @@ pub fn build(ui: &mut egui::Ui, app: &mut App) {
             #[cfg(debug_assertions)]
             windows::DEBUG.menu_button_toggle(ui);
         });
+
+        ui.with_layout(egui::Layout::right_to_left(egui::Align::Center), |ui| {
+            #[cfg(target_arch = "wasm32")]
+            ui.hyperlink_to("Download the full version", env!("CARGO_PKG_HOMEPAGE"));
+
+            egui::warn_if_debug_build(ui);
+        });
     });
 }
