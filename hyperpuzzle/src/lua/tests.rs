31458d1fbffd1c410380a37c015fb290a4b4e710
HactarCE <6060305+HactarCE@users.noreply.github.com>
2024-04-17 19:39:02 -0400

    Fix one more warning

--- hyperpuzzle/src/lua/tests.rs
+++ hyperpuzzle/src/lua/tests.rs
@@ -4,6 +4,6 @@ use crate::library::LibraryDb;
 #[test]
 fn run_lua_tests() {
     let db = LibraryDb::new();
-    let (logger, log_rx) = LuaLogger::new();
+    let (logger, _log_rx) = LuaLogger::new();
     LuaLoader::new(db, logger).run_test_suite("tests.lua", include_str!("tests.lua"));
 }
