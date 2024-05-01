1e3727460be200b551cc18ed6df18072ea20326a
HactarCE <6060305+HactarCE@users.noreply.github.com>
2024-05-01 18:53:46 -0400

    Delete orphaned file stubs

--- hyperpuzzle/src/puzzle/projection_type.rs
+++
@@ -1,5 +0,0 @@
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Hash)]
-pub enum ProjectionType {
-    _3D,
-    _4D,
-}
--- hyperspeedcube/src/input.rs
+++
@@ -1,3 +0,0 @@
-/// Modifiers, keys pressed, held commands, etc.
-#[derive(Debug, Default, Copy, Clone)]
-pub struct Input {}
