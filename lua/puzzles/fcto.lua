f89bd44dd4d9c855e09ab8cc568aed77f7a735f9
HactarCE <6060305+HactarCE@users.noreply.github.com>
2024-07-21 22:40:11 -0400

    Clean up CTO and add FCTO

--- lua/puzzles/fcto.lua
+++ lua/puzzles/fcto.lua
@@ -1,9 +1,9 @@
 local utils = require('utils')
 local symmetries = require('symmetries')
 
-puzzles:add('cto', {
+puzzles:add('fcto', {
   ndim = 3,
-  name = "Corner-Turning Octahedron",
+  name = "Face-And-Corner-Turning Octahedron",
   colors = 'octahedron',
   build = function(self)
     local sym = cd'bc3'
@@ -11,9 +11,13 @@ puzzles:add('cto', {
     self:carve(shape:iter_poles())
 
     -- Define axes and slices
-    self.axes:add(sym:orbit(sym.oox.unit), {2/3 * sqrt(3), 1/3 * sqrt(3)})
+    self.axes:add(shape:iter_poles(), utils.layers_exclusive(1, -1, 3))
+    self.axes:add(sym:orbit(sym.oox.unit), utils.layers_exclusive(sqrt(3), 0, 3))
 
     -- Define twists
+    for _, axis, twist_transform in sym.chiral:orbit(self.axes[sym.xoo.unit], sym:thru(2, 3)) do
+      self.twists:add(axis, twist_transform)
+    end
     for _, axis, twist_transform in sym.chiral:orbit(self.axes[sym.oox.unit], sym:thru(2, 1)) do
       self.twists:add(axis, twist_transform)
     end
