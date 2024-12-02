7d35224e935f0035b1f16b2820aabb8befa09273
HactarCE <6060305+HactarCE@users.noreply.github.com>
2024-12-01 22:33:46 -0500

    Add hyperbolic trig functions to Lua API

--- hyperpuzzle/src/lua/prelude/utils.lua
+++ hyperpuzzle/src/lua/prelude/utils.lua
@@ -9,6 +9,17 @@ math.tau = math.pi * 2
 math.degree = math.pi / 180
 math.phi = (1 + math.sqrt(5)) / 2
 
+-- Add hyperbolic trig
+local exp = math.exp
+local log = math.log
+local sqrt = math.sqrt
+math.sinh = function(x) return 0.5 * (exp(x) - exp(-x)) end
+math.cosh = function(x) return 0.5 * (exp(x) + exp(-x)) end
+math.tanh = function(x) return (exp(2*x) - 1) / (exp(2*x) + 1) end
+math.asinh = function(x) return log(x + sqrt(x*x + 1)) end
+math.acosh = function(x) return log(x + sqrt(x*x - 1)) end
+math.atanh = function(x) return 0.5 * log((1+x)/(1-x)) end
+
 function string.fmt2(s1, s2, ...)
   return string.format(s1, ...), string.format(s2, ...)
 end
