1a9291ae07ce2c982ac5657c07e88cbac296f72a
HactarCE <6060305+HactarCE@users.noreply.github.com>
2025-01-06 11:16:07 -0500

    Add HMT links

---
+++ lua/puzzles/hmt.lua
@@ -0,0 +1,6 @@
+-- alias of Obieemnaia Logicheskaia Igra https://twistypuzzles.com/app/museum/museum_showitem.php?pkey=9618
+-- also BM Pyraminx: https://twistypuzzles.com/app/museum/museum_showitem.php?pkey=8418
+
+-- main link: https://twistypuzzles.com/app/museum/museum_showitem.php?pkey=547
+
+-- "royal" tetrahedron: https://twistypuzzles.com/app/museum/museum_showitem.php?pkey=2006
