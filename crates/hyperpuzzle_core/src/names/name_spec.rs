d1d21276d2d1af4a3c31442612fa7f3d6bc8ae43
HactarCE <6060305+HactarCE@users.noreply.github.com>
2026-01-15 21:55:23 -0500

    Add simplices

--- crates/hyperpuzzle_core/src/names/name_spec.rs
+++ crates/hyperpuzzle_core/src/names/name_spec.rs
@@ -1,7 +1,8 @@
 //! Name specifications, used for puzzle elements such as axes, twists, and
 //! colors.
 //!
-//! TODO: revamp this; use only `_` for separators, and link to `SiGN` standards
+//! TODO: revamp this; use only `_` for separators (`__` etc. for
+//! lower-precedence separator), and link to `SiGN` standards
 //!
 //! - Whitespace characters are never allowed
 //! - Characters other than whitespace or ASCII punctuation are always literal
