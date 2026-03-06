6707e8f63c11bea026d3baf14000a8882f185b51
HactarCE <6060305+HactarCE@users.noreply.github.com>
2026-03-05 21:17:12 -0500

    Log level `info` by default

--- crates/hyperspeedcube/src/main.rs
+++ crates/hyperspeedcube/src/main.rs
@@ -53,7 +53,7 @@ fn main() -> eyre::Result<()> {
     use clap::Parser;
 
     // Initialize logging.
-    env_logger::builder().init();
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let args = cli::Args::parse();
 
